1e1ac47e2df99cb7eb99cdf8a5943a37d3837f99
Toya Takahashi <toyatakahashi522@gmail.com>
2024-11-24 16:34:18 -0500

    second iteration complete. not working without zero rotation and angular velocity constraints

--- toy_solver.py
+++ toy_solver.py
@@ -1,5 +1,6 @@
 import cvxpy as cp
 import numpy as np
+import pandas as pd 
 
 # Outer label: landmark number
 # Inner label: timestep
@@ -7,9 +8,9 @@ measurements = {
     # Landmark number
     0: {
         # Timestep number
-        0: np.array([[10,3,1]]).T,
-        1: np.array([[6,3,1]]).T,
-        2: np.array([[2,3,1]]).T,
+        0: np.array([[10,-3,1]]).T,
+        1: np.array([[6,-3,1]]).T,
+        2: np.array([[2,-3,1]]).T,
     },
 }
 
@@ -106,17 +107,22 @@ print("Q - translation block:")
 print(Q[18*N-9+3*K:, 18*N-9+3*K:])
 print()
 
+# PSD constraint
 constraints = [X >> 0]
 
 # Start point has 0 rotation (identity matrix)
 for i in range(9*N-9, 9*N):
     for j in range(dim_x):
         A = np.zeros((dim_x, dim_x))
-        A[i,j] = 1
-        A[j,i] = 1
-        if (i == j == 9*N-9) or (i == j == 9*N-5) or (i == j == 9*N-1):
+        if i == j:
+            A[i,j] = 1
+            A[j,i] = 1
+        else:
+            A[i,j] = 0.5
+            A[j,i] = 0.5
+        if (i % 9 == 0 or i % 9 == 4 or i % 9 == 8) and (j % 9 == 0 or j % 9 == 4 or j % 9 == 8) and j < 18*N-9:
             constraints.append(cp.trace(A @ X) == 1)
-        if (i != 9*N-9) and (i != 9*N-5) and (i != 9*N-1):
+        if (i % 9 != 0) and (i % 9 != 4) and (i % 9 != 8):
             constraints.append(cp.trace(A @ X) == 0)
 
 # Start point has 0 translation
@@ -142,20 +148,21 @@ for i in range(9*N-9):
         elif (i % 9 != 0) and (i % 9 != 4) and (i % 9 != 8):
             constraints.append(cp.trace(A @ X) == 0)
 
-# TODO: REMOVE. ZERO ANGULAR VELOCITY TEST
-for i in range(9*N-9, 18*N-9):
-    for j in range(dim_x):
-        A = np.zeros((dim_x, dim_x))
-        if i == j:
-            A[i,j] = 1
-            A[j,i] = 1
-        else:
-            A[i,j] = 0.5
-            A[j,i] = 0.5
-        if (i % 9 == 0 or i % 9 == 4 or i % 9 == 8) and (j % 9 == 0 or j % 9 == 4 or j % 9 == 8) and j < 18*N-9:
-            constraints.append(cp.trace(A @ X) == 1)
-        elif (i % 9 != 0) and (i % 9 != 4) and (i % 9 != 8):
-            constraints.append(cp.trace(A @ X) == 0)
+## TODO: REMOVE. ZERO ROTATION TEST
+#for i in range(9*N-9, 18*N-9):
+#    for j in range(dim_x):
+#        A = np.zeros((dim_x, dim_x))
+#        if i == j:
+#            A[i,j] = 1
+#            A[j,i] = 1
+#        else:
+#            A[i,j] = 0.5
+#            A[j,i] = 0.5
+#        if (i % 9 == 0 or i % 9 == 4 or i % 9 == 8) and (j % 9 == 0 or j % 9 == 4 or j % 9 == 8) and j < 18*N-9:
+#            constraints.append(cp.trace(A @ X) == 1)
+#        elif (i % 9 != 0) and (i % 9 != 4) and (i % 9 != 8):
+#            constraints.append(cp.trace(A @ X) == 0)
+
 # R^TR=I constraints
 for t in range(N):
     for i in range(3):
@@ -178,79 +185,58 @@ for t in range(N):
 
 # Translation odometry constraints
 for t in range(N - 1):
-    A = np.zeros((dim_x, dim_x))
-    A[9*N-9+9*t+0, 18*N-9+3*K+3*t+0] = 0.5  # R_i[0,0]*t_i[0]
-    A[9*N-9+9*t+3, 18*N-9+3*K+3*t+1] = 0.5  # R_i[1,0]*t_i[1]
-    A[9*N-9+9*t+6, 18*N-9+3*K+3*t+2] = 0.5  # R_i[2,0]*t_i[2]
-    A[9*N-9+9*t+0, 18*N-9+3*K+3*t+3] = -0.5 # -R_i[0,0]*t_{i+1}[0]
-    A[9*N-9+9*t+3, 18*N-9+3*K+3*t+4] = -0.5 # -R_i[1,0]*t_{i+1}[1]
-    A[9*N-9+9*t+6, 18*N-9+3*K+3*t+5] = -0.5 # -R_i[2,0]*t_{i+1}[2]
+    for j in range(3):
+        A = np.zeros((dim_x, dim_x))
+        A[9*N-9+9*t+0+j, 18*N-9+3*K+3*t+0] = 0.5  # R_i[0,0]*t_i[0]
+        A[9*N-9+9*t+3+j, 18*N-9+3*K+3*t+1] = 0.5  # R_i[1,0]*t_i[1]
+        A[9*N-9+9*t+6+j, 18*N-9+3*K+3*t+2] = 0.5  # R_i[2,0]*t_i[2]
+        A[9*N-9+9*t+0+j, 18*N-9+3*K+3*t+3] = -0.5 # -R_i[0,0]*t_{i+1}[0]
+        A[9*N-9+9*t+3+j, 18*N-9+3*K+3*t+4] = -0.5 # -R_i[1,0]*t_{i+1}[1]
+        A[9*N-9+9*t+6+j, 18*N-9+3*K+3*t+5] = -0.5 # -R_i[2,0]*t_{i+1}[2]
 
-    # Transposes
-    A[18*N-9+3*K+3*t+0, 9*N-9+9*t+0] = 0.5
-    A[18*N-9+3*K+3*t+1, 9*N-9+9*t+3] = 0.5
-    A[18*N-9+3*K+3*t+2, 9*N-9+9*t+6] = 0.5
-    A[18*N-9+3*K+3*t+3, 9*N-9+9*t+0] = -0.5
-    A[18*N-9+3*K+3*t+4, 9*N-9+9*t+3] = -0.5
-    A[18*N-9+3*K+3*t+5, 9*N-9+9*t+6] = -0.5
+        # Transposes
+        A[18*N-9+3*K+3*t+0, 9*N-9+9*t+0+j] = 0.5
+        A[18*N-9+3*K+3*t+1, 9*N-9+9*t+3+j] = 0.5
+        A[18*N-9+3*K+3*t+2, 9*N-9+9*t+6+j] = 0.5
+        A[18*N-9+3*K+3*t+3, 9*N-9+9*t+0+j] = -0.5
+        A[18*N-9+3*K+3*t+4, 9*N-9+9*t+3+j] = -0.5
+        A[18*N-9+3*K+3*t+5, 9*N-9+9*t+6+j] = -0.5
 
-    d = np.zeros((dim_v, 1))
-    d[3*t,0] = 1 # v_i[0]
-    constraints.append(cp.trace(A @ X) + d.T @ v == 0)
-
-    A = np.zeros((dim_x, dim_x))
-    A[9*N-9+9*t+1, 18*N-9+3*K+3*t+0] = 0.5  # R_i[0,1]*t_i[0]
-    A[9*N-9+9*t+4, 18*N-9+3*K+3*t+1] = 0.5  # R_i[1,1]*t_i[1]
-    A[9*N-9+9*t+7, 18*N-9+3*K+3*t+2] = 0.5  # R_i[2,1]*t_i[2]
-    A[9*N-9+9*t+1, 18*N-9+3*K+3*t+3] = -0.5 # -R_i[0,1]*t_{i+1}[0]
-    A[9*N-9+9*t+4, 18*N-9+3*K+3*t+4] = -0.5 # -R_i[1,1]*t_{i+1}[1]
-    A[9*N-9+9*t+7, 18*N-9+3*K+3*t+5] = -0.5 # -R_i[2,1]*t_{i+1}[2]
-
-    # Transposes
-    A[18*N-9+3*K+3*t+0, 9*N-9+9*t+1] = 0.5
-    A[18*N-9+3*K+3*t+1, 9*N-9+9*t+4] = 0.5
-    A[18*N-9+3*K+3*t+2, 9*N-9+9*t+7] = 0.5
-    A[18*N-9+3*K+3*t+3, 9*N-9+9*t+1] = -0.5
-    A[18*N-9+3*K+3*t+4, 9*N-9+9*t+4] = -0.5
-    A[18*N-9+3*K+3*t+5, 9*N-9+9*t+7] = -0.5
-
-    d = np.zeros((dim_v, 1))
-    d[3*t+1,0] = 1 # v_i[1]
-    constraints.append(cp.trace(A @ X) + d.T @ v == 0)
-
-    A = np.zeros((dim_x, dim_x))
-    A[9*N-9+9*t+2, 18*N-9+3*K+3*t+0] = 0.5  # R_i[0,2]*t_i[0]
-    A[9*N-9+9*t+5, 18*N-9+3*K+3*t+1] = 0.5  # R_i[1,2]*t_i[1]
-    A[9*N-9+9*t+8, 18*N-9+3*K+3*t+2] = 0.5  # R_i[2,2]*t_i[2]
-    A[9*N-9+9*t+2, 18*N-9+3*K+3*t+3] = -0.5 # -R_i[0,2]*t_{i+1}[0]
-    A[9*N-9+9*t+5, 18*N-9+3*K+3*t+4] = -0.5 # -R_i[1,2]*t_{i+1}[1]
-    A[9*N-9+9*t+8, 18*N-9+3*K+3*t+5] = -0.5 # -R_i[2,2]*t_{i+1}[2]
-
-    # Transposes
-    A[18*N-9+3*K+3*t+0, 9*N-9+9*t+2] = 0.5
-    A[18*N-9+3*K+3*t+1, 9*N-9+9*t+5] = 0.5
-    A[18*N-9+3*K+3*t+2, 9*N-9+9*t+8] = 0.5
-    A[18*N-9+3*K+3*t+3, 9*N-9+9*t+2] = -0.5
-    A[18*N-9+3*K+3*t+4, 9*N-9+9*t+5] = -0.5
-    A[18*N-9+3*K+3*t+5, 9*N-9+9*t+8] = -0.5
-
-    d = np.zeros((dim_v, 1))
-    d[3*t+2,0] = 1 # v_i[2]
-    constraints.append(cp.trace(A @ X) + d.T @ v == 0)
+        d = np.zeros((dim_v, 1))
+        d[3*t+j,0] = 1 # v_i[0]
+        constraints.append(cp.trace(A @ X) + d.T @ v == 0)
 
 # Rotation odometry constraints
+for t in range(N - 2):
+    for j in range(3):
+        for i in range(3):
+            A = np.zeros((dim_x, dim_x))
+            A[9*N-9+9*t+0+3*j, 9*t+0+i] = 0.5      # R_t[j,0]*Omega_t[0,i]
+            A[9*N-9+9*t+1+3*j, 9*t+3+i] = 0.5      # R_t[j,1]*Omega_t[1,i]
+            A[9*N-9+9*t+2+3*j, 9*t+6+i] = 0.5      # R_t[j,2]*Omega_t[2,i]
+            A[9*N-9+9*t+18+3*j, 9*t+9+3*i] = -0.5  # R_{t+2}[j,0]*Omega_{t+1}[i,0]
+            A[9*N-9+9*t+19+3*j, 9*t+10+3*i] = -0.5 # R_{t+2}[j,1]*Omega_{t+1}[i,1]
+            A[9*N-9+9*t+20+3*j, 9*t+11+3*i] = -0.5 # R_{t+2}[j,2]*Omega_{t+1}[i,2]
+
+            # Transposes
+            A[9*t+0+i, 9*N-9+9*t+0+3*j] = 0.5
+            A[9*t+3+i, 9*N-9+9*t+1+3*j] = 0.5
+            A[9*t+6+i, 9*N-9+9*t+2+3*j] = 0.5
+            A[9*t+9+3*i, 9*N-9+9*t+18+3*j] = -0.5
+            A[9*t+10+3*i, 9*N-9+9*t+19+3*j] = -0.5
+            A[9*t+11+3*i, 9*N-9+9*t+20+3*j] = -0.5
+
+            constraints.append(cp.trace(A @ X) == 0)
 
 # Problem definition
 prob = cp.Problem(cp.Minimize(cp.trace(Q @ X) + cp.quad_form(v, P)), constraints)
 prob.solve(solver=cp.MOSEK)
 
 # Print result
-np.set_printoptions(suppress=True,
-   formatter={'float_kind':'{:0.5f}'.format})
+np.set_printoptions(threshold=np.inf, suppress=True,
+    formatter={'float_kind':'{:0.4f}'.format})
 print("\nThe optimal value is", prob.value)
 print(prob.status)
-print()
-print("Rank of X is", np.linalg.matrix_rank(X.value))
 print()
 print("A solution X is")
 print("Angular velocity part:")
@@ -266,6 +252,13 @@ print()
 # Reconstruct x
 U, S, Vt = np.linalg.svd(X.value)
 x = U[:, 0] * np.sqrt(S[0])
+if x[0] < 0:
+    x = -x
 print("Reconstructed x is")
 print(x)
+print("Rank of X is", np.linalg.matrix_rank(X.value, tol=1e-6, hermitian=True))
+print()
+
+DF = pd.DataFrame(X.value) 
+DF.to_csv("test.csv")
 
